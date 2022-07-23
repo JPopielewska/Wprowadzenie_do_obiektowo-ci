14ae98c2857cd82fa9732c0e2e73013f91ce0dc3
JPopielewska <juliapopielewska@op.pl>
2022-07-23 15:37:40 +0200

    Car

---
+++ src/Car.java
@@ -0,0 +1,13 @@
+public class Car {
+    private String kolor;
+    private int  liczbaDrzwi;
+    private String marka;
+    private boolean aktumatycznaSkrzynia;
+
+    public Car(String kolor, int liczbaDrzwi, String marka, boolean automatycznaSkrzynia){
+        this.kolor = kolor;
+        this.liczbaDrzwi = liczbaDrzwi;
+        this.marka = marka;
+        this.aktumatycznaSkrzynia = automatycznaSkrzynia;
+    }
+}
