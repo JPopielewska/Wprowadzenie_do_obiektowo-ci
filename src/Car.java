fdd9f2085df917f367858514f321d628733131c4
JPopielewska <juliapopielewska@op.pl>
2022-07-24 09:35:33 +0200

    Car 2

--- src/Car.java
+++ src/Car.java
@@ -2,12 +2,37 @@ public class Car {
     private String kolor;
     private int  liczbaDrzwi;
     private String marka;
-    private boolean aktumatycznaSkrzynia;
+    private boolean automatycznaSkrzynia;
 
     public Car(String kolor, int liczbaDrzwi, String marka, boolean automatycznaSkrzynia){
         this.kolor = kolor;
         this.liczbaDrzwi = liczbaDrzwi;
         this.marka = marka;
-        this.aktumatycznaSkrzynia = automatycznaSkrzynia;
+        this.automatycznaSkrzynia = automatycznaSkrzynia;
+    }
+    public void setautomatycznaSkrzynia(boolean aktumatycznaSkrzynia) {
+        this.automatycznaSkrzynia = automatycznaSkrzynia;
+    }
+    public boolean getautomatycznaSkrzynia(){
+        return automatycznaSkrzynia;
+    }
+
+    public void setkolor(String kolor) {
+        this.kolor = kolor;
+    }
+    public String getkolor(){
+        return kolor;
+    }
+    public void setliczbaDrzwi (int liczbaDrzwi) {
+        this.liczbaDrzwi = liczbaDrzwi;
+    }
+    public int getliczbaDrzwi(){
+        return liczbaDrzwi;
+    }
+    public void setmarka (String marka){
+        this.marka = marka;
+    }
+    public String getmarka (){
+        return marka;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
+        Car audi = new Car("Czewrony",5, "A4", false);
+        System.out.println(audi.getautomatycznaSkrzynia());
+        audi.setautomatycznaSkrzynia(true);
+        System.out.println(audi.getautomatycznaSkrzynia());
+
+        Car ford = new Car("Czarny", 5, "Focus", false);
+        System.out.println(ford.getkolor());
+        ford.setkolor("Szary");
+        System.out.println(ford.getkolor());
     }
 }
